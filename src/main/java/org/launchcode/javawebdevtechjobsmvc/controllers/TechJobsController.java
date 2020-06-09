a9f07ea1dca30bb7ef61a985f0c214e66ae503da
bhs8574 <bhs8574@gmail.com>
2020-06-08 19:09:44 -0500

    Started (barely) on the Super Bonus Mission.  Will probably change the look of my pages to not look just like the demo app so I can change my table html to not be terrible to look at.

---
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/TechJobsController.java
@@ -0,0 +1,13 @@
+package org.launchcode.javawebdevtechjobsmvc.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.HashMap;
+
+public class TechJobsController {
+
+    static HashMap<String, String> actionChoices = new HashMap<>();
+
+
+}
